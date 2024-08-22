8268a9dc6bc664c65b6e53eb5a1777121f018fd9
Pankaj Kumar Upadhyay <kashyap7161731@gmail.com>
2024-08-22 14:11:30 +0530

    let's begin

---
+++ resources/react/Welcome.jsx
@@ -0,0 +1,10 @@
+// resources/react/Welcome.jsx
+import React from 'react';
+function Welcome() {
+return (
+<div className="welcome">
+<h1>Welcome to Your Laravel and React.js App!</h1>
+</div>
+);
+}
+export default Welcome;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Laravel and React.js App</title>
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">  </head>
+<body>
+<div id="root"></div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
